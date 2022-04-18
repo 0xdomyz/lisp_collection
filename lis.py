8ef5a1d7f265a03a91852365897af2dbc0adb41a
unknown <yzdominic@outlook.com>
2022-04-18 13:25:04 +1000

    reformat lis

--- lis.py
+++ lis.py
@@ -5,17 +5,18 @@
 import math
 import operator as op
 
-Symbol = str              # A Scheme Symbol is implemented as a Python str
-Number = (int, float)     # A Scheme Number is implemented as a Python int or float
-Atom   = (Symbol, Number) # A Scheme Atom is a Symbol or Number
-List   = list             # A Scheme List is implemented as a Python list
-Exp    = (Atom, List)     # A Scheme expression is an Atom or List
-Env    = dict             # A Scheme environment (defined below) 
-                          # is a mapping of {variable: value}
+Symbol = str  # A Scheme Symbol is implemented as a Python str
+Number = (int, float)  # A Scheme Number is implemented as a Python int or float
+Atom = (Symbol, Number)  # A Scheme Atom is a Symbol or Number
+List = list  # A Scheme List is implemented as a Python list
+Exp = (Atom, List)  # A Scheme expression is an Atom or List
+Env = dict  # A Scheme environment (defined below)
+# is a mapping of {variable: value}
+
 
 def parse(program: str) -> Exp:
     """Read a Scheme expression from a string.
-    
+
     Examples
     ----------
     >>> parse("(begin (define r 10) (* pi (* r r)))")
@@ -23,127 +24,151 @@ def parse(program: str) -> Exp:
     """
     return read_from_tokens(tokenize(program))
 
+
 def tokenize(chars: str) -> list:
     """Convert a string into a list of tokens.
-    
+
     Examples
     --------------
     >>> tokenize("(begin (define r 10) (* pi (* r r)))")
     ['(', 'begin', '(', 'define', 'r', '10', ')', '(', '*', 'pi', '(', '*', 'r', 'r', ')', ')', ')']
     """
-    return chars.replace('(', ' ( ').replace(')', ' ) ').split()
+    return chars.replace("(", " ( ").replace(")", " ) ").split()
+
 
 def read_from_tokens(tokens: list) -> Exp:
     """Read an expression from a sequence of tokens."""
     if len(tokens) == 0:
-        raise SyntaxError('unexpected EOF')
+        raise SyntaxError("unexpected EOF")
     token = tokens.pop(0)
-    if token == '(':
+    if token == "(":
         L = []
-        while tokens[0] != ')':
+        while tokens[0] != ")":
             L.append(read_from_tokens(tokens))
-        tokens.pop(0) # pop off ')'
+        tokens.pop(0)  # pop off ')'
         return L
-    elif token == ')':
-        raise SyntaxError('unexpected )')
+    elif token == ")":
+        raise SyntaxError("unexpected )")
     else:
         return atom(token)
 
+
 def atom(token: str) -> Atom:
     """Numbers become numbers; every other token is a symbol."""
-    try: return int(token)
+    try:
+        return int(token)
     except ValueError:
-        try: return float(token)
+        try:
+            return float(token)
         except ValueError:
             return Symbol(token)
+
 
 def standard_env() -> Env:
     """An environment with some Scheme standard procedures."""
     env = Env()
-    env.update(vars(math)) # sin, cos, sqrt, pi, ...
-    env.update({
-        '+':op.add, '-':op.sub, '*':op.mul, '/':op.truediv, 
-        '>':op.gt, '<':op.lt, '>=':op.ge, '<=':op.le, '=':op.eq, 
-        'abs':     abs,
-        'append':  op.add,  
-        'apply':   lambda proc, args: proc(*args),
-        'begin':   lambda *x: x[-1],
-        'car':     lambda x: x[0],
-        'cdr':     lambda x: x[1:], 
-        'cons':    lambda x,y: [x] + y,
-        'eq?':     op.is_, 
-        'expt':    pow,
-        'equal?':  op.eq, 
-        'length':  len, 
-        'list':    lambda *x: List(x), 
-        'list?':   lambda x: isinstance(x, List), 
-        'map':     map,
-        'max':     max,
-        'min':     min,
-        'not':     op.not_,
-        'null?':   lambda x: x == [], 
-        'number?': lambda x: isinstance(x, Number),  
-		'print':   print,
-        'procedure?': callable,
-        'round':   round,
-        'symbol?': lambda x: isinstance(x, Symbol),
-    })
+    env.update(vars(math))  # sin, cos, sqrt, pi, ...
+    env.update(
+        {
+            "+": op.add,
+            "-": op.sub,
+            "*": op.mul,
+            "/": op.truediv,
+            ">": op.gt,
+            "<": op.lt,
+            ">=": op.ge,
+            "<=": op.le,
+            "=": op.eq,
+            "abs": abs,
+            "append": op.add,
+            "apply": lambda proc, args: proc(*args),
+            "begin": lambda *x: x[-1],
+            "car": lambda x: x[0],
+            "cdr": lambda x: x[1:],
+            "cons": lambda x, y: [x] + y,
+            "eq?": op.is_,
+            "expt": pow,
+            "equal?": op.eq,
+            "length": len,
+            "list": lambda *x: List(x),
+            "list?": lambda x: isinstance(x, List),
+            "map": map,
+            "max": max,
+            "min": min,
+            "not": op.not_,
+            "null?": lambda x: x == [],
+            "number?": lambda x: isinstance(x, Number),
+            "print": print,
+            "procedure?": callable,
+            "round": round,
+            "symbol?": lambda x: isinstance(x, Symbol),
+        }
+    )
     return env
+
 
 class Env(dict):
     """An environment: a dict of {'var': val} pairs, with an outer Env."""
+
     def __init__(self, parms=(), args=(), outer=None):
         self.update(zip(parms, args))
         self.outer = outer
+
     def find(self, var):
         "Find the innermost Env where var appears."
         return self if (var in self) else self.outer.find(var)
 
+
 class Procedure(object):
     """A user-defined Scheme procedure."""
+
     def __init__(self, parms, body, env):
         self.parms, self.body, self.env = parms, body, env
-    def __call__(self, *args): 
+
+    def __call__(self, *args):
         return eval(self.body, Env(self.parms, args, self.env))
+
 
 global_env = standard_env()
 
+
 def eval(x, env=global_env):
     """Evaluate an expression in an environment.
-    
+
     Examples
     --------------
     >>> eval(parse("(begin (define r 10) (* pi (* r r)))"))
     314.1592653589793
     """
-    if isinstance(x, Symbol):    # variable reference
+    if isinstance(x, Symbol):  # variable reference
         return env.find(x)[x]
-    elif not isinstance(x, List):# constant 
-        return x   
-    op, *args = x       
-    if op == 'quote':            # quotation
+    elif not isinstance(x, List):  # constant
+        return x
+    op, *args = x
+    if op == "quote":  # quotation
         return args[0]
-    elif op == 'if':             # conditional
+    elif op == "if":  # conditional
         (test, conseq, alt) = args
-        exp = (conseq if eval(test, env) else alt)
+        exp = conseq if eval(test, env) else alt
         return eval(exp, env)
-    elif op == 'define':         # definition
+    elif op == "define":  # definition
         (symbol, exp) = args
         env[symbol] = eval(exp, env)
-    elif op == 'set!':           # assignment
+    elif op == "set!":  # assignment
         (symbol, exp) = args
         env.find(symbol)[symbol] = eval(exp, env)
-    elif op == 'lambda':         # procedure
+    elif op == "lambda":  # procedure
         (parms, body) = args
         return Procedure(parms, body, env)
-    else:                        # procedure call
+    else:  # procedure call
         proc = eval(op, env)
         vals = [eval(arg, env) for arg in args]
         return proc(*vals)
 
-def repl(prompt='lis.py> '):
+
+def repl(prompt="lis.py> "):
     """A prompt-read-eval-print loop.
-    
+
     Examples
     ------------
     >>> repl()
@@ -167,7 +192,7 @@ def repl(prompt='lis.py> '):
     41369087205782.695
     lis.py> (define first car)
     lis.py> (define rest cdr)
-    lis.py> (define count (lambda (item L) (if L (+ (equal? item (first L)) (count item (rest L))) 0))) 
+    lis.py> (define count (lambda (item L) (if L (+ (equal? item (first L)) (count item (rest L))) 0)))
     lis.py> (count 0 (list 0 1 2 3 0 0))
     3
     lis.py> (count (quote the) (quote (the more the merrier the bigger the better)))
@@ -197,12 +222,17 @@ def repl(prompt='lis.py> '):
     """
     while True:
         val = eval(parse(input(prompt)))
-        if val is not None: 
+        if val is not None:
             print(lispstr(val))
+
 
 def lispstr(exp):
     """Convert a Python object back into a Lisp-readable string."""
     if isinstance(exp, List):
-        return '(' + ' '.join(map(lispstr, exp)) + ')' 
+        return "(" + " ".join(map(lispstr, exp)) + ")"
     else:
         return str(exp)
+
+
+if __name__ == "__main__":
+    repl()
